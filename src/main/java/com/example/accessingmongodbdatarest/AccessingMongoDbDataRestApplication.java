ca4ac029ae86e5c60324ffd32d55c24854f640ee
Mario Perez-Jordan <mpjmario@gmail.com>
2022-01-21 04:03:08 -0500

    Initial commit.Spring application that lets you create and retrieve Person objects stored in a MongoDB NoSQL database by using Spring Data REST.Click Dependencies and select Rest Repositories and Spring Data MongoDB.

---
+++ src/main/java/com/example/accessingmongodbdatarest/AccessingMongoDbDataRestApplication.java
@@ -0,0 +1,14 @@
+package com.example.accessingmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongoDbDataRestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AccessingMongoDbDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accessingmongodbdatarest/AccessingMongoDbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMongoDbDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
